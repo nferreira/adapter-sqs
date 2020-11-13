f3d95c81e580d0ae694efaecc11f1af7693e9f4c
Nadilson Ferreira <nadilson@route.com>
2020-11-13 20:54:35 -0300

    Changes...

--- go.mod
+++ go.mod
@@ -5,6 +5,6 @@ go 1.15
 require (
 	github.com/aws/aws-sdk-go v1.35.24
 	github.com/gofiber/fiber/v2 v2.1.4
-	github.com/nferreira/adapter v0.1.20
-	github.com/nferreira/app v0.1.20
+	github.com/nferreira/adapter v0.1.21
+	github.com/nferreira/app v0.1.21
 )
--- pkg/adapter/sqs/sqs_adapter.go
+++ pkg/adapter/sqs/sqs_adapter.go
@@ -1,16 +1,17 @@
-package fiber
+package sqs
 
 import (
 	"context"
+	"encoding/base64"
+	"encoding/json"
 	"errors"
-	"os"
-	"reflect"
+	"fmt"
 	"strings"
 	"time"
 
-	f "github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/compress"
-	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
 	"github.com/nferreira/adapter/pkg/adapter"
 	"github.com/nferreira/app/pkg/app"
 	"github.com/nferreira/app/pkg/env"
@@ -18,10 +19,12 @@ import (
 )
 
 const (
-	Headers       = service.HeadersField
-	Body          = service.BodyField
-	CorrelationId = "Correlation-Id"
-	AdapterId     = "fiber"
+	Headers        = service.HeadersField
+	Body           = service.BodyField
+	CorrelationId  = "Correlation-Id"
+	AdapterId      = "sqs"
+	MessageIdField = "MessageId"
+	MessageField   = "Message"
 )
 
 var (
@@ -29,218 +32,260 @@ var (
 )
 
 type Params map[string]interface{}
-type Handler func(path string, handlers ...f.Handler) f.Router
-type GetParams func(fiberRule *BindingRule,
-	businessService *service.BusinessService,
-	c *f.Ctx) (*Params, error)
 
 type Adapter struct {
-	app      app.App
-	fiberApp *f.App
+	app                 app.App
+	client              *sqs.SQS
+	queueURL            *string
+	visibilityTimeout   int64
+	maxNumberOfMessages int64
+	waitTimeSeconds     int64
+	readMessageTimeout  time.Duration
+	businessService     service.BusinessService
+	debug               bool
+	shutdownChannel     chan bool
 }
 
 func New() adapter.Adapter {
+	debug := env.GetBool("DEBUG", false)
+
 	return &Adapter{
-		app:      nil,
-		fiberApp: newFiber(),
+		app:    nil,
+		client: newClient(),
+		debug:  debug,
 	}
 }
 
 func (a *Adapter) BindRules(rules map[adapter.BindingRule]service.BusinessService) {
-	for rule, businessService := range rules {
-		fiberRule := rule.(*BindingRule)
-		if fiberRule.Method == Get {
-			bind(fiberRule, a, &businessService, a.fiberApp.Get, getParams)
-		} else if fiberRule.Method == Post {
-			bind(fiberRule, a, &businessService, a.fiberApp.Post, getPayload)
-		} else if fiberRule.Method == Put {
-			bind(fiberRule, a, &businessService, a.fiberApp.Put, getPayload)
-		} else if fiberRule.Method == Patch {
-			bind(fiberRule, a, &businessService, a.fiberApp.Patch, getPayload)
-		} else if fiberRule.Method == Delete {
-			bind(fiberRule, a, &businessService, a.fiberApp.Delete, getParams)
-		} else if fiberRule.Method == Options {
-			bind(fiberRule, a, &businessService, a.fiberApp.Options, getParams)
-		}
+	if len(rules) == 0 {
+		panic("I have to have at least one queue to read from")
+	}
+
+	if len(rules) > 1 {
+		panic("I only support reading from one queue at the moment")
+	}
+
+	for _, businessService := range rules {
+		a.businessService = businessService
 	}
 }
 
 func (a *Adapter) Start(ctx context.Context) error {
-	application := ctx.Value("app")
-	switch application.(type) {
-	case app.App:
-		a.app = application.(app.App)
-	default:
-		panic("I need an application to live!!!")
-	}
-	return a.fiberApp.Listen(env.GetString("FIBER_HTTP_PORT", ":8080"))
+	a.app = configureApp(ctx)
+	a.queueURL = configureSqs(ctx, a)
+	a.visibilityTimeout = int64(env.GetInt("SQS_VISIBILITY_TIMEOUT", 60))
+	a.maxNumberOfMessages = int64(env.GetInt("SQS_MAX_NUMBER_OF_MESSAGES", 10))
+	a.readMessageTimeout = env.GetDuration("SQS_READ_MESSAGE_TIMEOUT_IN_SECONDS", time.Second, time.Second*60)
+	a.waitTimeSeconds = int64(env.GetInt("SQS_WAIT_TIME_SECONDS", 20))
+	go a.startWorker(ctx)
+	<-a.shutdownChannel
+	return nil
 }
 
 func (a *Adapter) Stop(_ context.Context) error {
-	return a.fiberApp.Shutdown()
+	a.shutdownChannel <- true
+	return nil
 }
 
 func (a *Adapter) CheckHealth(ctx context.Context) error {
 	return nil
 }
 
-func newFiber() *f.App {
-	fiberApp := f.New(f.Config{
-		Concurrency:     env.GetInt("FIBER_CONCURRENCY", 256*1024),
-		ReadTimeout:     env.GetDuration("FIBER_READ_TIMEOUT", time.Minute, time.Duration(3)*time.Minute),
-		WriteTimeout:    env.GetDuration("FIBER_WRITER_TIMEOUT", time.Minute, time.Duration(3)*time.Minute),
-		ReadBufferSize:  env.GetInt("FIBER_READ_BUFFER", 4096),
-		WriteBufferSize: env.GetInt("FIBER_WRITE_BUFFER", 4096),
-	})
-
-	loggerMiddleware := logger.New(logger.Config{
-		Next:         nil,
-		Format:       "[${status} - ${latency} ${method} ${path}\n",
-		TimeFormat:   "15:04:05",
-		TimeZone:     "Local",
-		TimeInterval: 500 * time.Millisecond,
-		Output:       os.Stderr,
-	})
-
-	fiberApp.Use(func(ctx *f.Ctx) error {
-		if ctx.Path() != "/health" {
-			return loggerMiddleware(ctx)
-		}
-		return nil
-	})
-
-	if env.GetBool("FIBER_USE_COMPRESSION", false) {
-		fiberApp.Use(compress.New(compress.Config{
-			Level: compress.LevelBestSpeed,
-		}))
-	}
-
-	return fiberApp
+func newClient() *sqs.SQS {
+	return sqs.New(session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	})))
 }
 
-func getParams(fiberRule *BindingRule, businessService *service.BusinessService, c *f.Ctx) (*Params, error) {
-	params := make(Params)
-	for _, param := range fiberRule.Params {
-		var value string
-		value = c.Params(param)
-		if strings.TrimSpace(value) == "" {
-			value = strings.TrimSpace(c.Query(param))
-		}
-		params[param] = value
+func configureApp(ctx context.Context) app.App {
+	application := ctx.Value("app")
+	switch application.(type) {
+	case app.App:
+		return application.(app.App)
+	default:
+		panic("I need an application to live!!!")
 	}
-	return &params, nil
 }
 
-func getPayload(fiberRule *BindingRule, businessService *service.BusinessService, c *f.Ctx) (params *Params, err error) {
-	params, err = getParams(fiberRule, businessService, c)
-	if err != nil {
-		return nil, err
+func configureSqs(ctx context.Context, adapter *Adapter) *string {
+	var urlResult *sqs.GetQueueUrlOutput
+	var err error
+	queueName := env.GetString("QUEUE_NAME", "")
+	if len(strings.TrimSpace(queueName)) == 0 {
+		panic("I need an queue name to start!!!")
 	}
-	serviceRequest := (*businessService).CreateRequest()
-	headers := make(map[string]string)
-	c.Request().Header.VisitAll(func(key []byte, value []byte) {
-		headers[string(key)] = string(value)
-	})
-	(*params)[Headers] = headers
-	err = c.BodyParser(&serviceRequest)
-	if err == nil {
-		(*params)[Body] = serviceRequest
-		return params, nil
+	if urlResult, err = adapter.client.GetQueueUrl(&sqs.GetQueueUrlInput{
+		QueueName: &queueName,
+	}); err != nil {
+		panic(fmt.Sprintf("Failed to get QueueUrl %s", err.Error()))
 	}
-	return nil, ErrBadPayload
+	return urlResult.QueueUrl
 }
 
-func bind(fiberRule *BindingRule,
-	a *Adapter,
-	businessService *service.BusinessService,
-	handler Handler,
-	getParams GetParams) {
+func (a *Adapter) startWorker(ctx context.Context) {
+	shutdownRequested := false
+	for !shutdownRequested {
+		select {
+		case shutdownRequested = <-a.shutdownChannel:
+			fmt.Printf("Shutdown Requested: %t\n", shutdownRequested)
+			return
+		default:
+			msgResult, err := a.readMessages(ctx)
+			if err != nil {
+				fmt.Printf("Error reading messages. Reason: [%s]\n", err.Error())
+			}
+			numberOfMessages := len(msgResult.Messages)
+			if a.debug {
+				fmt.Printf("Got %d messages\n", numberOfMessages)
+			}
+			for i, message := range msgResult.Messages {
+				if a.debug {
+					fmt.Printf("Processing %d of %d messages\n", i+1, numberOfMessages)
+				}
+				var correlationId *string
+				var request *service.Params
+				correlationId, request, err = a.parseMessage(message)
+				if err != nil {
+					fmt.Printf("Failed to parse message, due to error: [%s]. Message Payload: [%s]\n",
+						err.Error(),
+						sqsMessageToString(message))
+					a.client.DeleteMessage(&sqs.DeleteMessageInput{
+						QueueUrl:      a.queueURL,
+						ReceiptHandle: message.ReceiptHandle,
+					})
+					continue
+				}
 
-	handler(fiberRule.Path, func(c *f.Ctx) error {
-		params, err := getParams(fiberRule, businessService, c)
-		if err != nil {
-			a.handleResult(c,
-				service.
-					NewResultBuilder().
-					WithError(err).
-					Build(),
-				fiberRule)
-			return err
-		}
+				result := a.executeBusinessService(ctx, a.businessService, correlationId, request)
 
-		result, done := a.executeBusinessService(c, businessService, params, fiberRule)
-		if done {
-			return nil
-		}
-		a.handleResult(c, result, fiberRule)
-		return nil
-	})
-}
-
-func (a *Adapter) executeBusinessService(c *f.Ctx, businessService *service.BusinessService, params *Params, fiberRule *BindingRule) (*service.Result, bool) {
-	correlationId := GetCorrelationId(c)
-	executionContext := service.NewExecutionContext(correlationId, a.app)
-	ctx := context.WithValue(c.Context(), service.ExecutionContextKey, executionContext)
-	p := service.Params(map[string]interface{}(*params))
-	result := (*businessService).Execute(ctx, &p)
-	if result.Error != nil {
-		k := reflect.TypeOf(result.Error).Kind()
-		hashable := k < reflect.Array || k == reflect.Ptr || k == reflect.UnsafePointer
-		if hashable {
-			status, found := fiberRule.ErrorMapping[result.Error]
-			if found {
-				_ = c.SendStatus(status)
-			} else {
-				if result.Code == 0 {
-					_ = c.SendStatus(500)
+				if result.Error != nil {
+					fmt.Printf("Service execution failed, due to error: [%s]\n",
+						result.Error.Error())
 				} else {
-					_ = c.SendStatus(result.Code)
+					a.client.DeleteMessage(&sqs.DeleteMessageInput{
+						QueueUrl:      a.queueURL,
+						ReceiptHandle: message.ReceiptHandle,
+					})
 				}
 			}
-		} else {
-			if result.Code == 0 {
-				_ = c.SendStatus(500)
-			} else {
-				_ = c.SendStatus(result.Code)
-			}
 		}
-
-		return result, true
 	}
-	return result, false
 }
 
-func (a *Adapter) handleResult(c *f.Ctx, result *service.Result, fiberRule *BindingRule) {
-	if result.Error != nil {
-		status, found := fiberRule.ErrorMapping[result.Error]
-		if found {
-			_ = c.SendStatus(status)
-		} else {
-			if result.Code == 0 {
-				_ = c.SendStatus(500)
-			} else {
-				_ = c.SendStatus(result.Code)
-			}
-		}
-	} else {
-		if result.Code == 0 {
-			_ = c.SendStatus(500)
-		} else {
-			c.Status(result.Code)
-		}
-		for key, value := range result.Headers {
-			v, err := ToString(value)
-			if err != nil {
-				_ = c.Status(500).SendString(err.Error())
-				return
-			}
-			c.Set(key, *v)
-		}
-		if result.Response != nil {
-			err := c.JSON(result.Response)
-			if err != nil {
-				_ = c.SendStatus(500)
-			}
-		}
+func (a *Adapter) parseMessage(sqsMessage *sqs.Message) (correlationId *string, request *service.Params, err error) {
+	if a.debug {
+		fmt.Printf("Received Message=[%s]\n", sqsMessageToString(sqsMessage))
 	}
+
+	if sqsMessage.Body == nil {
+		fmt.Printf("Message Body is empty\n")
+		return nil, nil, err
+	}
+
+	var decodedBuff []byte
+	if decodedBuff, err = base64.StdEncoding.DecodeString(*sqsMessage.Body); err != nil {
+		// If for any reason the base64 decoding failed
+		// let's assume it is a regular string encoded JSON
+		decodedBuff = []byte(*sqsMessage.Body)
+	}
+
+	var parsedMessage map[string]interface{}
+	err = json.Unmarshal(decodedBuff, &parsedMessage)
+	if err != nil {
+		fmt.Printf("Failed to unmarshal message. Error: %s\n", err.Error())
+		return nil, nil, err
+	}
+
+	// we shall get the correlation id from on of these fields
+	// as on our local enrionment the first one is used
+	// but in prod using SNS the message is encoded in base64
+	if parsedMessage[MessageIdField] == nil && sqsMessage.MessageId == nil {
+		fmt.Println("Failed to get message id. It is empty")
+		return nil, nil, err
+	}
+
+	// we shall get the correlation id from on of these fields
+	// as on our local enrionment the first one is used
+	// but in prod using SNS the message is encoded in base64
+	if parsedMessage[MessageField] == nil &&
+		parsedMessage[service.BodyField] == nil {
+		fmt.Println("Failed to get message body. It is empty")
+		return nil, nil, err
+	}
+
+	var messageId *string = nil
+	var tmp string
+	var found bool
+	if tmp, found = parsedMessage[MessageIdField].(string); found {
+		messageId = &tmp
+	}
+	if messageId == nil {
+		messageId = sqsMessage.MessageId
+	}
+	correlationId = messageId
+
+	var encodedMessage *string = nil
+
+	if tmp, found = parsedMessage[MessageField].(string); found {
+		encodedMessage = &tmp
+		var messageBuffer []byte
+		messageBuffer, err = base64.StdEncoding.DecodeString(*encodedMessage)
+		if err != nil {
+			return nil, nil, err
+		}
+		encodedMessage = aws.String(string(messageBuffer))
+		if request, err = a.createAndPopulateRequest([]byte(*encodedMessage)); err != nil {
+			return nil, nil, err
+		}
+		return correlationId, request, nil
+	}
+
+	request = ToServiceParam(&parsedMessage)
+
+	return correlationId, request, nil
+}
+
+func ToServiceParam(o *map[string]interface{}) *service.Params {
+	p := service.Params(*o)
+	return &p
+}
+
+func (a *Adapter) createAndPopulateRequest(messageBuffer []byte) (request *service.Params, err error) {
+	tmp := a.businessService.CreateRequest().(map[string]interface{})
+	request = (*service.Params)(&tmp)
+	if err = json.Unmarshal(messageBuffer, &request); err != nil {
+		return nil, err
+	}
+	return request, nil
+}
+
+func (a *Adapter) executeBusinessService(ctx context.Context, businessService service.BusinessService, correlationId *string, params *service.Params) *service.Result {
+	executionContext := service.NewExecutionContext(*correlationId, a.app)
+	execCtx := context.WithValue(ctx, service.ExecutionContextKey, executionContext)
+	return a.businessService.Execute(execCtx, params)
+}
+
+func (a *Adapter) readMessages(ctx context.Context) (*sqs.ReceiveMessageOutput, error) {
+	newCtx, cancel := context.WithTimeout(ctx, a.readMessageTimeout)
+	defer cancel()
+	return a.client.ReceiveMessageWithContext(newCtx, &sqs.ReceiveMessageInput{
+		AttributeNames: []*string{
+			aws.String(sqs.MessageSystemAttributeNameSentTimestamp),
+		},
+		MessageAttributeNames: []*string{
+			aws.String(sqs.QueueAttributeNameAll),
+		},
+		QueueUrl:            a.queueURL,
+		MaxNumberOfMessages: &a.maxNumberOfMessages,
+		VisibilityTimeout:   &a.visibilityTimeout,
+		WaitTimeSeconds:     &a.waitTimeSeconds,
+	})
+}
+
+func sqsMessageToString(message *sqs.Message) string {
+	result, err := json.Marshal(message)
+	if err != nil {
+		return fmt.Sprintf("<FAILED TO MARSHAL>: Reason: %s", err.Error())
+	}
+	return string(result)
 }
